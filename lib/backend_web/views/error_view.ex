1637552ab9bf93d9bfc286b32aa09aac1d202c6b
Mario Cassiano <mariocassiano@gmail.com>
2022-11-02 12:16:09 -0300

    feat: add error view

--- lib/backend_web/views/error_view.ex
+++ lib/backend_web/views/error_view.ex
@@ -5,9 +5,6 @@ defmodule BackendWeb.ErrorView do
 
   alias Ecto.Changeset
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
