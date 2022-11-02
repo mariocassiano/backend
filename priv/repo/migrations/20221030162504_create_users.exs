66fd4f524b4d5223b4111720e1119313d80ba591
Mario Cassiano <mariocassiano@gmail.com>
2022-11-02 12:23:28 -0300

    feat: add migration creat users

---
+++ priv/repo/migrations/20221030162504_create_users.exs
@@ -0,0 +1,16 @@
+defmodule Backend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :cpf, :string
+      add :address, :map, default: %{}, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+  end
+end
