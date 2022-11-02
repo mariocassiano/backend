ad04703ef9e7f9a074613d2ce8b338ac84aa9527
Mario Cassiano <mariocassiano@gmail.com>
2022-11-02 12:15:49 -0300

    feat: add error view

--- lib/backend_web/views/error_view.ex
+++ lib/backend_web/views/error_view.ex
@@ -1,11 +1,9 @@
 defmodule BackendWeb.ErrorView do
   use BackendWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
+  alias Ecto.Changeset
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
@@ -13,4 +11,23 @@ defmodule BackendWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("error.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
+  def render("error.json", %{result: error_message}) do
+    %{message: error_message}
+  end
+
+  def translate_errors(%Changeset{} = changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", translate_value(value))
+      end)
+    end)
+  end
+
+  # defp translate_value({:parameterized, Ecto.Enum, _map}), do: ""
+  defp translate_value(value), do: to_string(value)
 end
