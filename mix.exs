0d095275a1fa455b19015fa5ba2db8b8cce549cf
Mario Cassiano <mariocassiano@gmail.com>
2022-11-02 11:14:10 -0300

    chore: add tesla dependency

--- mix.exs
+++ mix.exs
@@ -55,7 +55,9 @@ defmodule Backend.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:sobelow, "~> 0.8", only: :dev},
       {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
-      {:ex_machina, "~> 2.7.0"}
+      {:ex_machina, "~> 2.7.0"},
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.17.0"}
     ]
   end
 
