c1c5c252bf879a66374ec6f6503a978522912b9d
Mario Cassiano <mariocassiano@gmail.com>
2022-11-02 11:15:33 -0300

    chore: add bypass dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Backend.MixProject do
       {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
       {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4.0"},
-      {:hackney, "~> 1.17.0"}
+      {:hackney, "~> 1.17.0"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
